2a52db3659d5bb00aa382711b8f146997373bc3d
curt-kim <curt.kim@daumkakao.com>
2017-02-23 14:12:39 +0900

    daum retina support

--- ol-daum.js
+++ ol-daum.js
@@ -1,11 +1,13 @@
+// ol.source.XYZ
 ol.source.DaumImg = function(opt_options) {
   var options = opt_options || {};
-  if( !options.tileVersion) options.tileVersion = '165fbd'
+  if( !options.tileVersion) options.tileVersion = '4bauto'
   var resolutions = [];
   for(var i = 14; i >= 1; i--)
     resolutions.push(Math.pow(2, i-3))
   var maxExtent = [-30000-Math.pow(2,19)*2, -60000-Math.pow(2,19), -30000+Math.pow(2,19)*3, -60000+Math.pow(2, 19)*3]; // minx miny maxx maxy
   ol.source.TileImage.call(this, {
+    tilePixelRatio: options.tilePixelRatio,
     tileGrid: new ol.tilegrid.TileGrid({
       extent: maxExtent,
       origin: [-30000, -60000],
@@ -19,7 +21,8 @@ ol.source.DaumImg = function(opt_options) {
       //console.log(coordinate, z);
       var idx = x % 4;
       if(idx < 0 ) idx = idx+4;
-      var url = 'http://map' + idx +'.daumcdn.net/map_2d/'+ options.tileVersion +'/L'+z+'/'+ y +'/'+x +'.png';
+      var map_type = options.tilePixelRatio && options.tilePixelRatio >=2 ? 'map_2d_hd' : 'map_2d';
+      var url = 'http://map' + idx +'.daumcdn.net/' + map_type + '/'+ options.tileVersion +'/L'+z+'/'+ y +'/'+x +'.png';
       return url;
     }
   });
