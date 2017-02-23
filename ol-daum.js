ca46cecffe3cf028ccdfd8412e2fb96d792c900e
curt-kim <curt.kim@daumkakao.com>
2017-02-23 14:17:43 +0900

    retina를 자동 detect하도록 수정

--- ol-daum.js
+++ ol-daum.js
@@ -1,4 +1,7 @@
-// ol.source.XYZ
+function is_high_resolution_screen() {
+  return window.devicePixelRatio > 1;
+}
+
 ol.source.DaumImg = function(opt_options) {
   var options = opt_options || {};
   if( !options.tileVersion) options.tileVersion = '4bauto'
@@ -6,8 +9,9 @@ ol.source.DaumImg = function(opt_options) {
   for(var i = 14; i >= 1; i--)
     resolutions.push(Math.pow(2, i-3))
   var maxExtent = [-30000-Math.pow(2,19)*2, -60000-Math.pow(2,19), -30000+Math.pow(2,19)*3, -60000+Math.pow(2, 19)*3]; // minx miny maxx maxy
+  var tilePixelRatio = options.tilePixelRatio || is_high_resolution_screen() ? 2 : 1;
   ol.source.TileImage.call(this, {
-    tilePixelRatio: options.tilePixelRatio,
+    tilePixelRatio: tilePixelRatio,
     tileGrid: new ol.tilegrid.TileGrid({
       extent: maxExtent,
       origin: [-30000, -60000],
@@ -21,7 +25,7 @@ ol.source.DaumImg = function(opt_options) {
       //console.log(coordinate, z);
       var idx = x % 4;
       if(idx < 0 ) idx = idx+4;
-      var map_type = options.tilePixelRatio && options.tilePixelRatio >=2 ? 'map_2d_hd' : 'map_2d';
+      var map_type = tilePixelRatio >=2 ? 'map_2d_hd' : 'map_2d';
       var url = 'http://map' + idx +'.daumcdn.net/' + map_type + '/'+ options.tileVersion +'/L'+z+'/'+ y +'/'+x +'.png';
       return url;
     }
